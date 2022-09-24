d323532731687730f62c6d4da58321a05587a281
Lucas Casal Goncalves Do Rego <dustwestcool@gmail.com>
2022-09-24 18:32:54 +0200

    Programador Web

--- PROYECTO SITIO/routes/admin/login.js
+++ PROYECTO SITIO/routes/admin/login.js
@@ -9,7 +9,7 @@ router.get("/", function (req, res, next) {
 });
 
 /*para destruir variables de session*/
-router.get("/logut", function (req, res, next) {
+router.get("/logout", function (req, res, next) {
   req.session.destroy(); //destruir
   res.render("admin/login", {
     layout: "admin/layout",
