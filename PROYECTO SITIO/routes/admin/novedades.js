b148fbb74fa295213e93a1988d98947feaf24464
Lucas Casal Goncalves Do Rego <dustwestcool@gmail.com>
2022-10-08 14:50:16 +0200

    Programador Web

--- PROYECTO SITIO/routes/admin/novedades.js
+++ PROYECTO SITIO/routes/admin/novedades.js
@@ -15,9 +15,54 @@ router.get("/", async function (req, res, next) {
 /*Para eliminar una novedad*/
 router.get("/eliminar/:id", async (req, res, next) => {
   var id = req.params.id;
-  
+
   await novedadesModel.deleteNovedadesById(id);
   res.redirect("/admin/novedades");
 }); //cierra get de eliminar
+
+/*para una novedades NUEVA*/
+
+router.get("/agregar", (req, res, next) => {
+  res.render("admin/agregar", {
+    //agregar.hbs
+    layout: "admin/layout",
+  }); //cierra render
+}); //cierra Get
+
+router.post("/agregar", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    if (
+      req.body.titulo != "" &&
+      req.body.subtitulo != "" &&
+      req.body.cuerpo != ""
+    ) {
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect("/admin/novedades");
+    } else {
+      res.render("admin/agregar", {
+        layout: "admin/layout",
+        error: true,
+        message: "Todos los campos son requeridos",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.render("admin/agregar", {
+      layout: "admin/layout",
+      error: true,
+      message: "No se cargo la novedad",
+    });
+  }
+});
+
+router.get("/modificar/:id", async (req, res, next) => {
+  let id = req.params.id;
+  let novedad = await novedadesModel.getNovedadById(id);
+  res.render("admin/modificar", {
+    layout: "admin/layout",
+    novedad
+  });
+}); //cierro get modificar
 
 module.exports = router;
