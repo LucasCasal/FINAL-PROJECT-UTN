8ea9d4bd68be4d8fc8154f24802204fb81dc6f7d
Lucas Casal Goncalves Do Rego <dustwestcool@gmail.com>
2022-10-19 05:32:58 +0200

    Proyecto Final

--- M3U1/M3U1b/prueba.js
+++
@@ -1,3 +0,0 @@
-const moment = require('moment');
-
-console.log('Naci '+moment('03/11/1992','DD/MM/YYYY').fromNow());
--- PROYECTO SITIO/routes/admin/novedades.js
+++ PROYECTO SITIO/routes/admin/novedades.js
@@ -8,7 +8,13 @@ const destroy = util.promisify(cloudinary.uploader.destroy);
 
 /*listar las novedades*/
 router.get("/", async function (req, res, next) {
-  var novedades = await novedadesModel.getNovedades();
+  var novedades;
+
+  if (req.query.q === undefined) {
+    novedades = await novedadesModel.getNovedades();
+  } else {
+    novedades = await novedadesModel.buscarNovedades(req.query.q);
+  }
 
   novedades = novedades.map((novedad) => {
     if (novedad.img_id) {
@@ -33,6 +39,8 @@ router.get("/", async function (req, res, next) {
     layout: "admin/layout",
     usuario: req.session.nombre,
     novedades,
+    is_search: req.query.q !== undefined,
+    q: req.query.q,
   });
 });
 
